26f27ccfdb4ecc1e06e72126639f216a6617ea56
Maxson <77611206+Maxson71@users.noreply.github.com>
2024-07-23 07:37:38 +0000

    error

--- components/Frame.tsx
+++ components/Frame.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 type FrameProps = {
   title: string;
 };
 
 const Frame: React.FC<FrameProps> = (props) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
     <div
-      className="bg-foreground text-background py-3 pl-5 pr-8 
-                    text-sm leading-tight text-center uppercase"
+      ref={ref}
+      className={`bg-foreground text-background py-3 pl-5 pr-8 
+                  text-sm leading-tight text-center uppercase
+                  transition-opacity duration-1000 ease-out
+                  ${inView ? "opacity-100" : "opacity-0"}`}
     >
       {props.title}
     </div>
