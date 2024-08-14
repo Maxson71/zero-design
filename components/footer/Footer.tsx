71f1b59b48d9f4b349e7cce44fa11f66215311b3
Maxson <77611206+Maxson71@users.noreply.github.com>
2024-08-14 05:54:45 +0000

    footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,11 +3,124 @@ import Link from "next/link";
 import { CiSearch } from "react-icons/ci";
 import { PiShoppingCartThin } from "react-icons/pi";
 
+const data = {
+  sections: [
+    {
+      title: "Landing Pages",
+      links: [
+        {
+          title: "Landing 1",
+        },
+        {
+          title: "Landing 2",
+        },
+        {
+          title: "Landing 3",
+        },
+        {
+          title: "Landing 4",
+        }
+      ]
+    },
+    {
+      title: "Company",
+      links: [
+        {
+          title: "About 1",
+        },
+        {
+          title: "About 2",
+        },
+        {
+          title: "Contact 1",
+        },
+        {
+          title: "Contact 2",
+        },
+        {
+          title: "Contact 3",
+        },
+        {
+          title: "Team",
+        },
+        {
+          title: "FAQ",
+        }
+      ]
+    },
+    {
+      title: "CMS Pages",
+      links: [
+        {
+          title: "Works",
+        },
+        {
+          title: "Blog 1",
+        },
+        {
+          title: "Blog 2",
+        },
+        {
+          title: "Blog 3",
+        },
+        {
+          title: "Shop 1",
+        },
+        {
+          title: "Shop 2",
+        },
+        {
+          title: "Careers",
+        },
+        {
+          title: "Single product page",
+        },
+        {
+          title: "Single post page",
+        },
+        {
+          title: "Single careers page",
+        },
+        {
+          title: "Single work page",
+        }
+      ]
+    },
+    {
+      title: "Utility",
+      links: [
+        {
+          title: "Coming Soon",
+        },
+        {
+          title: "Password",
+        },
+        {
+          title: "Privacy Policy",
+        },
+        {
+          title: "Terms And Conditions",
+        },
+        {
+          title: "404",
+        },
+        {
+          title: "Licensing",
+        },
+        {
+          title: "Style Guide",
+        }
+      ]
+    }
+  ]
+};
+
+
+
 const Footer = () => {
   return (
-    <footer className="footer z-10 top-0 flex w-full items-center justify-between px-16 py-4 shadow-2xl h-appbar-height bg-background">
-      <div className="flex flex-row space gap-8">
-        <Logo size={0.95} />
+    <footer className="footer z-10 top-0 flex gap-8 w-full items-center px-16 py-4 shadow-2xl bg-background">
+      <div className="flex flex-col gap-8">
         <nav className="flex items-center gap-4 ">
           <Link
             href="#landing"
@@ -15,36 +128,23 @@ const Footer = () => {
           >
             Landing
           </Link>
-          <Link
-            href="/"
-            className="text-lg leading-tight hover:text-warning duration-300"
-          >
-            Company
-          </Link>
-          <Link
-            href="/"
-            className="text-lg leading-tight hover:text-warning duration-300"
-          >
-            CMS Pages
-          </Link>
-          <Link
-            href="/"
-            className="text-lg leading-tight hover:text-warning duration-300"
-          >
-            Utility
-          </Link>
         </nav>
       </div>
-      <div className="flex items-center gap-4">
-        <Link href="/">
-          <div className="button">$129 Buy Now</div>
-        </Link>
-        <Link href="/">
-          <CiSearch className="hover:text-warning  duration-300 cursor-pointer h-6 w-6" />
-        </Link>
-        <Link href="/">
-          <PiShoppingCartThin className="hover:text-warning  duration-300 cursor-pointer h-6 w-6" />
-        </Link>
+      <div className="flex gap-4 flex-row flex-wrap">
+        {data.sections.map((section, sectionIndex) => (
+          <div key={sectionIndex} className="flex flex-col gap-4">
+            <p className="text-2xl leading-loose">
+              {section.title}
+            </p>
+            <div className='flex flex-col flex-wrap gap-2'>
+              {(section.links).map((link, linkIndex) => (
+                <p key={linkIndex} className="text-base leading-tight text-gray-800">
+                  {link.title}
+                </p>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </footer>
   );
